3de650c6a00947806c4acb8106e7ae3ef6c7c8bb
Maccbrainy <iketakumichaelnedum@gmail.com>
2021-03-28 01:34:41 +0100

    CustomSettingsApi class registered

---
+++ inc/Api/CustomSettingsApi.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Custom Settings Api 
+ * 
+ * @package SeedwpsPlugin
+ */
+namespace Inc\Api;
+
+class CustomSettingsApi
+{
+
+	/**
+	 * [seedwps_get_the_taxonomies Retrieves post custom categories/taxonomies]
+	 * @return [returns results from the custom taxonomies defined]
+	 */
+	public static function seedwps_get_the_taxonomies(){
+
+        $taxonomy = 'software_category';
+        $terms = get_the_terms(get_the_ID(), $taxonomy);
+
+	    $separator = ' ';
+	    $output = '';
+	    $i = 1;
+
+	    if (!empty($terms)):
+	        foreach ($terms as $term):
+	            if ($i > 1): 
+	            $output .= $separator;
+	    endif;
+
+	    $output .= esc_html($term->slug);
+	    ++$i;
+	    endforeach;
+	    endif;
+
+	    return $output;
+	}
+
+	public static function portfolio_logo_uploader_field( $name, $value='')
+	{
+
+	    $image = ' button">Upload Project Logo';
+	    $image_size = 'full'; // it would be better to use thumbnail size here (150x150 or so)
+	    $display = 'none'; // display state of the "Remove image" button
+
+	    if( $image_attributes = wp_get_attachment_image_src( $value, $image_size ) ) {
+
+	        // $image_attributes[0] - image URL
+	        // $image_attributes[1] - image width
+	        // $image_attributes[2] - image height
+
+	        $image = '"><img src="' . $image_attributes[0] . '" style="max-width:50%;display:block;" />';
+
+	    } 
+
+	    return '
+	    <div>
+	        <a href="#" class="portfolio_upload_image_button' . $image . '</a>
+
+	        <input type="hidden" name="' . $name . '" id="' . $name . '" value="' . $value. '" />
+	        <a href="#" class="portfolio_remove_image_button" style="display:inline-block">Remove image</a>
+	    </div>'; 
+
+	}
+
+	public static function portfolio_project_logo_display( $name, $value='')
+	{
+
+	    $image = '">No project logo';
+	    $image_size = 'full'; // it would be better to use thumbnail size here (150x150 or so)
+	    $display = 'none'; // display state ot the "Remove image" button
+
+	    if( $image_attributes = wp_get_attachment_image_src( $value, $image_size ) ) {
+
+	        $image = '"><img src="' . $image_attributes[0] . '" style="max-width:70%;display:inline-block;" />';
+	    } 
+
+	    return '
+	    <div class="img-thumb">
+	        <span class="portfolio_upload_image_button' . $image . '</span>
+	    </div>';
+	}
+
+}
